873b1954651fa8e76634069904b650e7d443e418
Phelps Wener <phelipe_wener@hotmail.com>
2014-11-17 20:36:19 +0000

    Removendo comentários desnecessários

--- fractal.c
+++ fractal.c
@@ -10,39 +10,7 @@ int main(){
 	
 	printf("Quantos niveis quer criar: ");
 	scanf("%d", &size);
-	/*__fpurge(stdin);
 	
-	float P1x, P1y, P2x, P2y;
-	printf("Insira x de P1: ");
-	scanf("%f", &P1x);
-	__fpurge(stdin);
-	printf("\nInsira y de P1: ");
-	scanf("%f", &P1y);
-	__fpurge(stdin);	
-	printf("\nInsira x de P2: ");
-        scanf("%f", &P2x);
-	__fpurge(stdin);
-        printf("\nInsira y de P2: ");
-        scanf("%f", &P2y);
-	__fpurge(stdin);
-	
-	float P3x = 0.5*((P2x - P1x)/2)+(-0.866*(P2y - P1y)) - P1x;
-	float P3y = 0.866*((P2x - P1x)/2)+(0.5*(P2y - P1y)) + P1y;	
-	
-	printf("P1x = %f", P1x);
-	printf("P1y = %f", P1y);
-	printf("P3x = %f", P3x);
-	printf("P3y = %f", P3y);	
-
-	FILE *triangulo = fopen("map0.dat", "w");
-	fprintf(triangulo, "%f\t%f\n", P1x, P1y);
-	fprintf(triangulo, "%f\t%f\n", P2x, P2y); 
-	fprintf(triangulo, "%f\t%f\n", P3x, P3y);
-	fprintf(triangulo, "%f\t%f\n", P1x, P1y);
-	
-	fclose(triangulo);
-*/
-
 	// nome do arquivo a ser criado, ou map{n+1}.dat
 	char filename[size];
 	// nome do arquivo que serve de base ao que será criado, ou map{n}.dat
